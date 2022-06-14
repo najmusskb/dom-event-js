3e2d3e88f06da8c609527c223e65fb3f68eabee0
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-06-15 00:47:47 +0600

    first commit

---
+++ js/events.js
@@ -0,0 +1,18 @@
+function makered() {
+
+    document.body.style.backgroundColor = 'red';
+}
+
+function makeblue() {
+
+    document.body.style.backgroundColor = 'blue';
+}
+
+
+
+
+
+function makepink() {
+
+    document.body.style.backgroundColor = 'pink';
+}
